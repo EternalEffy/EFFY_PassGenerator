3006a189c7e963477658e06730bba5209443b082
EternalEffy <39722892+EternalEffy@users.noreply.github.com>
2021-02-24 00:44:26 +0300

    оставила только одну строку константу

--- src/EFFY_PassGenerator.java
+++ src/EFFY_PassGenerator.java
@@ -4,9 +4,7 @@ import java.util.Random;
 public class EFFY_PassGenerator {
     private int passLenght;
     private int passCount;
-    private final static String Digit = "0123456789"; //тут думаю может поменять метод цифрого пароля аналогично останым? из строки
-    private final static String Alpha = "AaBbCcDdEeFfGgHhJjKkIiLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
-    private final static String Combo = "AaBbCcDdEeFfGgHhJjKkIiLl0123456789MmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+    private final static String Combo = "0123456789AaBbCcDdEeFfGgHhJjKkIiLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
 
     public EFFY_PassGenerator(int passCount, int passLenght){
         this.passCount = passCount;
@@ -16,16 +14,16 @@ public class EFFY_PassGenerator {
     public ArrayList GenerateDigit(){
         ArrayList<String> passwordList = new ArrayList<>(passCount);
         StringBuffer sb = new StringBuffer(passLenght);
-            for (int i = 0; i<passCount;i++) {
-                for( int k = 0; k<passLenght;k++) {
-                    sb.append(new Random().nextInt(10));
-                }
-                if(!passwordList.contains(String.valueOf(sb))){
-                    passwordList.add(String.valueOf(sb));
-                    sb.delete(0,sb.length());
-                }
+        for (int i = 0; i < passCount; i++) {
+            for (int k = 0; k < passLenght; k++) {
+                sb.append(Combo.charAt((int)(10 * Math.random())));
+            }
+            if(!passwordList.contains(String.valueOf(sb))){
+                passwordList.add(String.valueOf(sb));
                 sb.delete(0,sb.length());
             }
+            sb.delete(0,sb.length());
+        }
         return passwordList;
     }
 
@@ -34,7 +32,7 @@ public class EFFY_PassGenerator {
         StringBuffer sb = new StringBuffer(passLenght);
         for (int i = 0; i < passCount; i++) {
             for (int k = 0; k < passLenght; k++) {
-                sb.append(Alpha.charAt((int)(Alpha.length() * Math.random())));
+                sb.append(Combo.substring(10).charAt((int)(Combo.substring(10).length()* Math.random())));
             }
             if(!passwordList.contains(String.valueOf(sb))){
                 passwordList.add(String.valueOf(sb));
